d0679f5f8a5d76c59b9e9e9dedd4a8ebc48a2ed4
Nicolás Von Muhlinen <nicovon24@gmail.com>
2023-06-02 16:28:26 -0300

    install and functionality email js

--- src/src-client/components/Landing/Contact/index.tsx
+++ src/src-client/components/Landing/Contact/index.tsx
@@ -1,9 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { ButtonSolid, InputTransparent } from "../../Styles/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+	faEnvelope,
+	faLocationDot,
+	faPhone,
+} from "@fortawesome/free-solid-svg-icons";
+import validateForm from "./validateForm";
+import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+	const initialInputs = {
+		name: "",
+		email: "",
+		subject: "",
+		message: "",
+	};
+
+	const [inputs, setInputs] = useState(initialInputs);
+	const [errors, setErrors] = useState({name: "El campo nombre debe estar completo"});
+
+	const handleChangeInput = (
+		e:
+			| React.ChangeEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLTextAreaElement>
+	) => {
+		const { name, value } = e.target;
+		setInputs({
+			...inputs,
+			[name]: value,
+		});
+		setErrors(
+			validateForm({
+				...inputs,
+				[name]: value,
+			})
+		);
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		const findError = Object.values(errors).find((err) => err !== "");
+
+		if (findError) {
+			Swal.fire({
+				title: "",
+				html: findError,
+				icon: "error",
+			});
+		} else {
+			emailjs
+				.send(
+					"service_2n12b5l", //service id
+					"template_eue0omc",
+					{
+						from_name: inputs?.name,
+						to_name: "Nicolás",
+						from_email: inputs?.email,
+						to_email: "nicovon24@gmail.com",
+						subject: inputs?.subject,
+						message: inputs?.message,
+					},
+					"wlCYUd_KEO_Z9vBR7"
+				)
+				.then(() => {
+					Swal.fire({
+						html: "Mensajo enviado con éxito",
+						icon: "success",
+					});
+					setInputs(initialInputs)
+					setErrors({name: "El campo nombre debe estar completo"})
+				});
+		}
+	};
+
 	return (
 		<div
 			className="w-full bg-violet-blue-landing py-5
@@ -46,31 +118,49 @@ const Contact = () => {
 				</div>
 
 				{/* form send message */}
-				<form className="px-4">
+				<form className="px-4" onSubmit={handleSubmit}>
 					<div className="grid lg:grid-cols-2 gap-4">
-						{/* email */}
-						<div className="flex flex-col items-center py-2">
-							<label className="text-black">Email</label>
+						{/* name */}
+						<div className="flex flex-col items-center">
+							<label className="text-black">Nombre</label>
 							<InputTransparent
 								type="text"
+								name="name"
+								placeholder="ej. jorge amuchastegui"
+								classes="w-full max-w-[350px] lg:max-w-[1000px]"
+								maxLength={40}
+								handleChange={handleChangeInput}
+								value={inputs?.name}
+							/>
+						</div>
+
+						{/* email */}
+						<div className="flex flex-col items-center">
+							<label className="text-black">Email</label>
+							<InputTransparent
+								type="email"
 								name="email"
 								placeholder="ej. al.dia.aplication@gmail.com"
 								classes="w-full max-w-[350px] lg:max-w-[1000px]"
 								maxLength={40}
+								handleChange={handleChangeInput}
+								value={inputs?.email}
 							/>
 						</div>
+					</div>
 
-						{/* asunto */}
-						<div className="flex flex-col items-center py-2">
-							<label className="text-black">Asunto</label>
-							<InputTransparent
-								type="text"
-								name="asunto"
-								placeholder="ej. consulta de planes"
-								classes="w-full max-w-[350px] lg:max-w-[1000px]"
-								maxLength={50}
-							/>
-						</div>
+					{/* asunto */}
+					<div className="flex flex-col items-center py-2">
+						<label className="text-black">Asunto</label>
+						<InputTransparent
+							type="text"
+							name="subject"
+							placeholder="ej. consulta de planes"
+							classes="w-full max-w-[350px] lg:max-w-[1000px]"
+							maxLength={50}
+							handleChange={handleChangeInput}
+							value={inputs?.subject}
+						/>
 					</div>
 
 					{/* mensaje */}
@@ -80,13 +170,18 @@ const Contact = () => {
 							className="w-full max-w-[350px] lg:max-w-[1000px] border-[1px] border-main-yellow text-gray-800 rounded-[12px] px-3 py-2 bg-transparent placeholder:text-gray-500
 							focus:outline-blue-600 text-sm"
 							placeholder="Texto"
+							name="message"
 							rows={6}
 							maxLength={3000}
+							onChange={handleChangeInput}
+							value={inputs?.message}
 						></textarea>
 					</div>
 
 					<div className="flex justify-center mt-4">
-						<ButtonSolid classes="w-24">Enviar</ButtonSolid>
+						<ButtonSolid classes="w-24" onClick={() => ""}>
+							Enviar
+						</ButtonSolid>
 					</div>
 				</form>
 				{/* whatsapp */}
--- src/src-client/components/Styles/Button.tsx
+++ src/src-client/components/Styles/Button.tsx
@@ -22,8 +22,10 @@ const InputTransparent = (props: any) => (
 		className={`border-[1px] border-main-yellow text-gray-800 text-sm rounded-[12px] px-3 py-2 bg-transparent placeholder:text-gray-500 focus:outline-blue-600
     ${props?.classes}`}
 		placeholder={props?.placeholder}
-		onClick={props?.handleClick}
+		onChange={props?.handleChange}
 		maxLength={props?.maxLength}
+		name={props?.name}
+		value={props?.value}
 	>
 		{props?.children}
 	</input>
