7dc82cbf7e4b53c075fc233691e0fd67c7ccc7f2
franco4457 <franco_forex@hotmail.com>
2023-06-13 17:18:06 -0300

    add actio to active user

--- src/src-client/components/Tables/AdminTable.tsx
+++ src/src-client/components/Tables/AdminTable.tsx
@@ -3,6 +3,8 @@ import SearchBar from '@components/generals/SeachBar'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import AdminModal from './AdminModal'
+import { requestAdminUsers } from '@/utils/request'
+
 
 const FILTER_TYPES = ['email', 'nombre']
 interface RequestUser {
@@ -12,25 +14,18 @@ interface RequestUser {
   users: UserWithId[]
 }
 
-const requetsUsers = async (): Promise<RequestUser> => {
-  const users = await fetch('/api/admin/users').then((res) => {
-    if (!res.ok) throw new Error(`Error: ${res.status}`)
-    return res.json()
-  })
-  return users as RequestUser
-}
 
-export default function AdminTable  ()  {
+export default function AdminTable() {
   const [users, setUSers] = useState<UserWithId[]>([])
 
   useEffect(() => {
     if (!users.length) {
-      console.log('requested users');
-      requetsUsers().then((users) => {
+      console.log('requested users')
+      requestAdminUsers<RequestUser>().then((users) => {
         setUSers(users.users)
       })
     }
-  }, [])// eslint-disable-line
+  }, []) // eslint-disable-line
 
   const onSubmit = (inputSearch: string, filterBy?: string) => {
     console.log('INPUT:', inputSearch)
@@ -44,32 +39,43 @@ export default function AdminTable  ()  {
           onSubmit={onSubmit}
           title="User List"
         />
-        <section className='mx-2 rounded-md shadow overflow-hidden'>
-          <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
-            <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
-              <tr className='[&>th]:p-2'>
-                <th className='w-10' >Imagen</th>
-                <th >Nombre</th>
-                <th >Email</th>
-                <th >Status</th>
-                <th >Actions</th>
+        <section className="mx-2 rounded-md shadow overflow-hidden">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr className="[&>th]:p-2">
+                <th className="w-10">Imagen</th>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th>Status</th>
+                <th>Plan</th>
+                <th>Actions</th>
               </tr>
             </thead>
             <tbody>
-              {
-                users.map((user, i) =>(<>
-                <tr key={i} className='bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600'>
-                  <th><Image src={user.image} alt='imagen del usuario' width='40' height='40' className='rounded-full m-auto' /></th>
-                  <th>{user.fullName}</th>
-                  <th>{user.email}</th>
-                  <th>{user.status}</th>
-                  <th>
-                    <AdminModal user={user} />
-                  </th>
-                </tr> 
+              {users.map((user, i) => (
+                <>
+                  <tr
+                    key={i}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  >
+                    <th>
+                      <Image
+                        src={user.image}
+                        alt="imagen del usuario"
+                        width="40"
+                        height="40"
+                        className="rounded-full m-auto"
+                      />
+                    </th>
+                    <th>{user.fullName}</th>
+                    <th>{user.email}</th>
+                    <th>{user.status.split(' - ')[0]}</th>
+                    <th>
+                      <AdminModal user={user} />
+                    </th>
+                  </tr>
                 </>
-                ))
-              }
+              ))}
             </tbody>
           </table>
         </section>
