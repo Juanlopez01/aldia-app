15a5fd5c5f77f8f6c3d8742eabc27de782b142d5
Nicolás Von Muhlinen <nicovon24@gmail.com>
2023-06-14 16:16:16 -0300

    some styles in admin

--- src/src-client/components/Tables/AdminTable.tsx
+++ src/src-client/components/Tables/AdminTable.tsx
@@ -30,6 +30,9 @@ export default function AdminTable() {
     console.log('INPUT:', inputSearch)
     console.log('FILTER:', filterBy)
   }
+
+  const stylesTH= "font-bold text-left uppercase align-middle bg-transparent border-b border-collapse shadow-none border-b-solid tracking-none whitespace-nowrap text-sm text-slate-400 opacity-70 dark:text-black"
+
   return (
     <>
       <section className="min-h-[75vh] w-full bg-light-green dark:bg-violet-blue-landing px-2 pt-8">
@@ -40,15 +43,15 @@ export default function AdminTable() {
         />
         <section className="mx-2 rounded-md shadow overflow-x-auto mt-4">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400 border-b-[1px] border-b-gray-300 dark:border-b-gray-100
-            py-6">
+            <thead className="text-xs text-gray-700 uppercase 
+            dark:text-gray-400 border-b-[1px] border-b-gray-300 dark:border-b-gray-100 bg-white">
               <tr className="[&>th]:px-2">
-                <th className="w-[60px] py-2">Imagen</th>
-                <th className='py-2'>Nombre</th>
-                <th className='py-2'>Email</th>
-                <th className='py-2'>Estado</th>
-                <th className='py-2'>Plan</th>
-                <th className='py-2'>Acciones</th>
+                <th className={`w-[60px] py-2 px-6 pl-2 ${stylesTH}`}>Imagen</th>
+                <th className={`py-3 px-6 pl-2 ${stylesTH}`}>Nombre</th>
+                <th className={`py-3 px-6 pl-2 ${stylesTH}`}>Email</th>
+                <th className={`py-3 px-6 pl-2 ${stylesTH}`}>Estado</th>
+                <th className={`py-3 px-6 pl-2 ${stylesTH}`}>Plan</th>
+                <th className={`py-3 px-6 pl-2 ${stylesTH}`}>Acciones</th>
               </tr>
             </thead>
             <tbody>
