ada6e6e85c305539208f758b8aa7bb6190ab2193
Nicolás Von Muhlinen <nicovon24@gmail.com>
2023-06-15 16:40:15 -0300

    pricing styles

--- src/pages/pricing/checkout/[plan].tsx
+++ src/pages/pricing/checkout/[plan].tsx
@@ -26,12 +26,12 @@ export default function Checkout() {
   return (
     <>
       <main className="flex justify-center bg-light-green dark:bg-violet-blue-landing">
-        <ul className="max-w-4xl m-0 p-0 flex-grow px-4 my-4">
+        <ul className="flex flex-wrap gap-4 justify-center m-0 p-0 flex-grow px-4 my-4">
           {/* 1. pay your subscription */}
-          <li className="md:flex flex-row w-full md:justify-between ">
-            <article className="md:w-1/2 [&>p]:m-0 flex flex-col justify-around">
+          <li className="max-w-[500px] flex flex-col items-start justify-around shadow-md px-4 rounded-lg">
+            <article className="[&>p]:m-0 mt-2 flex flex-col justify-around">
               <header className="">
-                <h1 className="text-4xl font-bold m-0 ">
+                <h1 className="text-4xl font-bold my-4 ">
                   <span className="text-medium-blue mr-2">1.</span> Paga tu
                   subscripción
                 </h1>
@@ -44,7 +44,7 @@ export default function Checkout() {
               </p>
               <p>CVU: alskdnoalds-asidnoasld-asd1901292os</p>
             </article>
-            <figure className="flex md:justify-center items-center m-auto">
+            <figure className="mt-3">
               <Image
                 src={QrSvg}
                 alt="QR to pay suscription"
@@ -55,10 +55,10 @@ export default function Checkout() {
           </li>
 
           {/* 2. notification */}
-          <li className="md:flex md:gap-16 flex-row-reverse w-full justify-between mt-4">
-            <article className="md:w-1/2 [&>p]:m-0 flex flex-col justify-around">
+          <li className="max-w-[500px] flex flex-col items-start justify-around shadow-md px-4 rounded-lg">
+            <article className="[&>p]:m-0 mt-2 flex flex-col justify-around">
               <header className="">
-                <h1 className="text-4xl font-bold m-0 ">
+                <h1 className="text-4xl font-bold m-0 my-4 ">
                   <span className="text-medium-blue mr-2">2.</span>
                   Envía un notificación
                 </h1>
@@ -69,21 +69,21 @@ export default function Checkout() {
               </p>
               <footer>Whatsapp: +69 1313 040613.</footer>
             </article>
-            <figure className="flex md:justify-center pt-4 items-center m-auto">
+            <figure className="flex pt-4">
               <Image
                 src="/wpp-image.png"
                 alt="QR to pay suscription"
-                height="400"
-                width="400"
+                height="350"
+                width="350"
               />
             </figure>
           </li>
 
           {/* 3. espera */}
-          <li className="md:flex flex-row w-full justify-between mt-8">
-            <article className="md:w-1/2 [&>p]:m-0 flex flex-col justify-around">
+          <li className="max-w-[500px] flex flex-col items-start justify-around shadow-md px-4 rounded-lg">
+            <article className="mt-2 [&>p]:m-0 flex flex-col justify-around">
               <header className="">
-                <h1 className="text-4xl font-bold m-0 ">
+                <h1 className="text-4xl font-bold m-0 my-4 ">
                   <span className="text-medium-blue mr-2 mb-2">3.</span>
                   Espera
                 </h1>
@@ -97,16 +97,16 @@ export default function Checkout() {
                 Dale click al siguiente boton 1 vez para notificar al
                 administrador que realizaste el pago.
               </p>
-              <footer className='grid max-w-[250px] mt-4 md:place-content-center'>
+              <footer className='grid max-w-[250px] mt-4'>
                 <NotifyBtn plan={plan} />
               </footer>
             </article>
-            <figure className="flex md:justify-center items-center m-auto">
+            <figure className="mt-4">
               <Image
                 src="/ilustration-wait.jpg"
                 alt="QR to pay suscription"
-                height="400"
-                width="400"
+                height="300"
+                width="300"
               />
             </figure>
           </li>
