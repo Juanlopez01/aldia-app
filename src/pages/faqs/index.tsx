f773072ab4cc9c73ffdc30ad0f02189e06892782
franco4457 <franco_forex@hotmail.com>
2023-06-18 18:46:15 -0300

    add minimal styles

--- src/pages/faqs/index.tsx
+++ src/pages/faqs/index.tsx
@@ -1,23 +1,63 @@
-import Contact from "@/src-client/components/Landing/Contact";
+import Contact from '@/src-client/components/Landing/Contact'
+import {
+  FAQuestion,
+  QuestionType,
+} from '@/src-client/components/faqs/FAQuestions'
 
-
+const questions: QuestionType[] = [
+  {
+    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
+    solution:
+      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
+    extra: 'Whatsapp: +53 123 345 1231235',
+  },
+  {
+    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
+    solution:
+      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
+    extra: 'Whatsapp: +53 123 345 1231235',
+  },
+  {
+    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
+    solution:
+      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
+    extra: 'Whatsapp: +53 123 345 1231235',
+  },
+  {
+    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
+    solution:
+      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
+    extra: 'Whatsapp: +53 123 345 1231235',
+  },
+]
 
 export default function PageFQAs() {
   return (
     <>
-    <main>
-        <header>
-            <h1>¡Hola!</h1>
-            <h2>¿En que podemos ayudarte?</h2>
-            <p>Esta es la pagina ayuda, preguntas frecuentes o FAQs{`(Frequently Asked Questions en inglés)`}</p>
-            </header> 
-            <section>
-
-            </section>
-            <footer>
-                <Contact/>
-            </footer>
-    </main>
+      <main className=" bg-light-green dark:bg-violet-blue-landing pt-12 flex flex-col justify-center items-center w-full">
+        <header className='text-center max-w-lg flex flex-col gap-2'>
+          <h1 className='m-0 text-4xl font-extrabold'>¡Hola!</h1>
+          <h2 className='m-0 text-xl font-bold'>¿En que podemos ayudarte?</h2>
+          <p className='m-0 text-base'>
+            Esta es la pagina ayuda, preguntas frecuentes o FAQs
+            {`(Frequently Asked Questions en inglés)`}
+          </p>
+          <p className='m-0 text-base'>
+            Si no encuetras una FAQ que solicione tu unconveniente, no dudes en
+            enviar un Email en la seccion de &lsquo;Contáctate con
+            nosotros&lsquo; y en campo de &lsquo;Asunto&lsquo; coloca el
+            problema que tuviste
+          </p>
+        </header>
+        <section className='max-w-2xl'>
+          {questions.map((question, i) => (
+            <FAQuestion key={i} {...question} />
+          ))}
+        </section>
+        <footer>
+          <Contact />
+        </footer>
+      </main>
     </>
   )
 }
--- src/src-client/components/faqs/FAQuestions.tsx
+++ src/src-client/components/faqs/FAQuestions.tsx
@@ -1,8 +1,26 @@
+import { useToggle } from '@/src-client/hooks/use-toggle'
 
-type QuestionProps ={
-    
+export type QuestionType = {
+  question: string
+  solution: string
+  extra?: string
 }
 
-export const FAQuestion=()=>{
-
+export const FAQuestion = ({ question, solution, extra }: QuestionType) => {
+  const { toggle, toggleHandler } = useToggle(false)
+  return (
+    <>
+      <article className='text-center m-2 shadow shadow-darkest-blue p-4 flex flex-col gap-2 w-full'>
+        <button onClick={toggleHandler}>
+          <h3 className='text-medium-blue text-xl font-semibold text-center m-0'>{question}</h3>
+        </button>
+        {toggle && (
+          <>
+            <p>{solution}</p>
+            {extra && <footer>{extra}</footer>}
+          </>
+        )}
+      </article>
+    </>
+  )
 }
