60255106fec9bf8123630c2d7cece5c4494e8507
Nicolás Von Muhlinen <nicovon24@gmail.com>
2023-06-18 15:38:50 -0300

    credits responsive

--- src/src-client/components/credit/SearchBarCredit.tsx
+++ src/src-client/components/credit/SearchBarCredit.tsx
@@ -7,19 +7,19 @@ const SearchBarCredit = ({filters, setFilters} : any) => {
     }
 
   return (
-    <div>
+    <div className='flex flex-wrap gap-x-6 gap-y-3'>
         <div className='flex flex-col'>
             <label>Tipo</label>
             <select name="type" id="type" defaultValue={'incomes'} onChange={onChange}
-            className='border-2 border-gray-300 rounded-md p-1 dark:bg-white  w-full md:w-1/2 xl:w-1/3'>
+            className='border-2 border-gray-300 rounded-md p-2 dark:bg-white w-[300px]'>
                 <option value="incomes" key='incomes'>Ingresos</option>
                 <option value="expenses" key='expenses'>Gastos</option>
             </select>
         </div>
-        <div className='flex flex-col pt-3'>
+        <div className='flex flex-col'>
             <label>Crédito</label>
             <select name='credit' id='credit' defaultValue={'Un pago'} onChange={onChange}
-            className='border-2 border-gray-300 rounded-md p-1 dark:bg-white w-full md:w-1/2 xl:w-1/3'>
+            className='border-2 border-gray-300 rounded-md p-2 dark:bg-white  w-[300px]'>
                 {creditList.map((credit : string) => {
                     return <option value={credit} key={credit}>{credit}</option>
                 })}
--- src/src-client/components/credit/TableCreditRow.tsx
+++ src/src-client/components/credit/TableCreditRow.tsx
@@ -13,8 +13,10 @@ const TableCreditRow = ({transaction} : Props) => {
     <>
         <tr key={transaction._id?.toString()} className='border-[1px] border-gray-300 border-b-black bg-gray-100 hover:bg-gray-200 duration-200'>
             <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md flex items-center gap-x-3'>
-					    <PhotoComponent category={cat!=="Mercadería" ? transaction?.category?.toString() : "Mercaderia"} width={30} height={30} />
-              {transaction.category}
+					    <div className='hidden sm:flex'>
+                <PhotoComponent category={cat!=="Mercadería" ? transaction?.category?.toString() : "Mercaderia"} width={30} height={30} />
+              </div>
+              <span>{transaction.category}</span>
             </td>
             <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md'>{transaction.description ? transaction.description : "No hay descripción"}</td>
             <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md'>{transaction.credit}</td>
