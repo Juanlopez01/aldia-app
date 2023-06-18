816e9850d43a82c78b762f669f9a3bade5ef8d05
franco4457 <franco_forex@hotmail.com>
2023-06-18 19:04:49 -0300

    populate questions

--- src/pages/faqs/index.tsx
+++ src/pages/faqs/index.tsx
@@ -12,21 +12,21 @@ const questions: QuestionType[] = [
     extra: 'Whatsapp: +53 123 345 1231235',
   },
   {
-    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
+    question: '¿No puedes acceder a tu cuenta?',
     solution:
-      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
+      'Si no puedes acceder a tu cuenta, no te procuepes, ve a la pagina de autenticación y preciona la opción de "Me olvidé mi constraseña", pon el email con el cual te registaste, esto te enviará un email en el cual te redigira a una pagina donde podras recuperar tu cuenta y elegir una nueva contraseña',
+    extra: 'Si lo aterior no funciona enviar un email con el asunto "RECOVER-ACCOUNT" en el cual expliques tu probelma en cuestion y sera soluciona en brevedad',
+  },
+  {
+    question: '¿Tienes un error de redirección o de datos erroneos?',
+    solution:
+      'Cuando quieres ir a una pagina te redirecciona a otra o la infomación que se muesta es errornea. Envia una email o un mesaje de whatsapp con el asunto de "ISSUE" con una descripción del error en cuestión y sera resuelto a la brevedad ',
     extra: 'Whatsapp: +53 123 345 1231235',
   },
   {
-    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
+    question: '¿Quieres que se agregue una funcionalidad?',
     solution:
-      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
-    extra: 'Whatsapp: +53 123 345 1231235',
-  },
-  {
-    question: '¿Has realizado un pago y no tienes el plan que pagaste?',
-    solution:
-      'La verificación del pago puede demorar alguno minutos o hasta horas, pero si ha pasado mas de 1 día comumnicate via email o whatsapp para agilizar la activación de tu plan',
+      'Envia una email o un mesaje de whatsapp con el asunto de "FEATURE" para que la funcionalidad sea agregada proximamente y ayudes a la aplicación a ser mas efieciente y util para los demás usuarios',
     extra: 'Whatsapp: +53 123 345 1231235',
   },
 ]
@@ -43,7 +43,7 @@ export default function PageFQAs() {
             {`(Frequently Asked Questions en inglés)`}
           </p>
           <p className='m-0 text-base'>
-            Si no encuetras una FAQ que solicione tu unconveniente, no dudes en
+            Si no encuetras una FAQ que solicione tu inconveniente, no dudes en
             enviar un Email en la seccion de &lsquo;Contáctate con
             nosotros&lsquo; y en campo de &lsquo;Asunto&lsquo; coloca el
             problema que tuviste
