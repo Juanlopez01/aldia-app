42589bca737aef0e8c3bea9b4b0ea1e398870fa8
Nicolás Von Muhlinen <nicovon24@gmail.com>
2023-06-18 15:23:30 -0300

    credits final styles

--- src/src-client/components/credit/TableCredit.tsx
+++ src/src-client/components/credit/TableCredit.tsx
@@ -7,23 +7,27 @@ interface Props {
     transactions: IncomeType[] | ExpenseType[]
 }
 
+const stylesTH =
+'px-6 pl-2  font-bold text-left uppercase align-middle bg-transparent border-b border-collapse shadow-none border-b-solid tracking-none whitespace-nowrap text-sm text-slate-400 opacity-70 dark:text-black'
+
 const TableCredit = ({transactions} : Props) => {
   return (
-    <section className='pt-4 overflow-x-auto'>
+    <section className='mt-4 overflow-x-auto rounded-[10px] w-full'>
+
         {transactions.length>0 
-        ? <table className='rounded-md shadow-plan'>
-            <thead className='bg-white border-[1px] border-gray-400'>
+        ? <table className='shadow-plan w-full'>
+            <thead className='border-[1px] border-gray-300 bg-white'>
                 <tr className=''>
-                    <th className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>Categoría</th>
-                    <th className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>Descripción</th>
-                    <th className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>Crédito</th>
-                    <th className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>Valor</th>
+                    <th className={`mob:px-2 md:px-8 py-3 text-sm md:text-md ${stylesTH}`}>Categoría</th>
+                    <th className={`mob:px-2 md:px-8 py-3 text-sm md:text-md ${stylesTH}`}>Descripción</th>
+                    <th className={`mob:px-2 md:px-8 py-3 text-sm md:text-md ${stylesTH}`}>Crédito</th>
+                    <th className={`mob:px-2 md:px-8 py-3 text-sm md:text-md ${stylesTH}`}>Valor</th>
                 </tr>
             </thead>
             <tbody>
                 {transactions.map((transaction) => { return <TableCreditRow key={transaction._id?.toString()} transaction={transaction}/>})}
             </tbody>
-        </table> : <p>No hay transacciones que coincidan</p>}
+        </table> : <p className='pl-4'>No hay transacciones que coincidan</p>}
     </section>
   )
 }
--- src/src-client/components/credit/TableCreditRow.tsx
+++ src/src-client/components/credit/TableCreditRow.tsx
@@ -1,19 +1,24 @@
 import { ExpenseType } from '@/models/expense.model';
 import { IncomeType } from '@/models/income.model';
 import React from 'react'
+import PhotoComponent from '../goals/PhotoComponent';
 
 interface Props {
     transaction: IncomeType | ExpenseType;
 }
 
 const TableCreditRow = ({transaction} : Props) => {
+  const cat= transaction?.category?.toString()
   return (
     <>
-        <tr key={transaction._id?.toString()} className='border-[1px] border-gray-400 bg-white'>
-            <td className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>{transaction.category}</td>
-            <td className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>{transaction.description ? transaction.description : "No hay descripción"}</td>
-            <td className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>{transaction.credit}</td>
-            <td className='mob:px-2 md:px-8 py-2 text-sm md:text-md'>${transaction.value}</td>
+        <tr key={transaction._id?.toString()} className='border-[1px] border-gray-300 border-b-black bg-gray-100 hover:bg-gray-200 duration-200'>
+            <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md flex items-center gap-x-3'>
+					    <PhotoComponent category={cat!=="Mercadería" ? transaction?.category?.toString() : "Mercaderia"} width={30} height={30} />
+              {transaction.category}
+            </td>
+            <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md'>{transaction.description ? transaction.description : "No hay descripción"}</td>
+            <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md'>{transaction.credit}</td>
+            <td className='mob:px-2 md:px-8 py-3 text-sm md:text-md'>${transaction.value}</td>
         </tr>
     </>
   )
