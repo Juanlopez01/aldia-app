2eeb9a0472b689068b73eb83b413c9a727a43795
Juan Lopez <juanpablolopez43@gmail.com>
2023-06-21 19:49:32 -0300

    fix checks

--- src/utils/creditFilter.ts
+++ src/utils/creditFilter.ts
@@ -9,10 +9,34 @@ interface PropsFilter {
 
 export const creditFilter = (incomes : IncomeType[], expenses : ExpenseType[], form : PropsFilter) => {
     if(form.type === 'expenses'){
-        const filter = expenses.filter((expense : ExpenseType) => expense.credit === form.credit)
+        const filter = expenses.filter((expense : ExpenseType) => {
+            let realExpenseCredit 
+            if(expense.credit && expense.credit.split(' ')[0] === 'No'){
+                realExpenseCredit = expense.credit.split(' ')[0];
+            } else if (expense.credit && expense.credit.split(' ')[0] !== 'No'){
+                realExpenseCredit = expense.credit.split(' ').slice(0,2).join(' ');
+            } else {
+                realExpenseCredit = 'No';
+            }
+
+            return realExpenseCredit === form.credit
+            
+        })
         return filter
     } else {
-        const filter = incomes.filter((income : IncomeType) => income.credit === form.credit)
+        const filter = incomes.filter((income : IncomeType) => {
+            let realExpenseCredit 
+            if(income.credit && income.credit.split(' ')[0] === 'No'){
+                realExpenseCredit = income.credit.split(' ')[0];
+            } else if (income.credit && income.credit.split(' ')[0] !== 'No'){
+                realExpenseCredit = income.credit.split(' ').slice(0,2).join(' ');
+            } else {
+                realExpenseCredit = 'No';
+            }
+
+            return realExpenseCredit === form.credit
+            
+        })
         return filter
     }
 }
